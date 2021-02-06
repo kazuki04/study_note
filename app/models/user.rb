222aab77a5c41318635be30ac6d37a0d81c3dc3c
kazuki04 <kazukik0402@gmail.com>
2021-02-06 21:38:15 +0900

    add sns_autorization servire-side

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,22 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :sns_credentials
+
+  def self.from_omniauth(auth_info)
+    sns = SnsCredential.where(provider: auth_info.provider, uid: auth.uid).first_or_create
+    
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email: auth.info.email
+    )
+    
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    
+    return {sns: sns, user: user}
+
+  end
+
 end
