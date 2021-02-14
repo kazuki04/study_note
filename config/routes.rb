f840aa7d59ed7b0550b2e065dcd80bff28a68850
kazuki04 <kazukik0402@gmail.com>
2021-02-14 10:38:17 +0900

    add association

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'goal_managements#show'
+  root 'calendars#show'
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
