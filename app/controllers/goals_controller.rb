d92126a62c4cbbac2220ea94f50c470606a03599
kazuki04 <kazukik0402@gmail.com>
2021-02-16 15:27:30 +0900

    add galo_func #create

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,9 +1,14 @@
 class GoalsController < ApplicationController
   def create
+    create_goal_names()
   end
 
   private
-  def goal_params
-    params.require(:goal).permit(goal_names:[])
+
+  def create_goal_names
+    goal_names = params.require(:goal)[:goal_names]
+    goal_names.each do |goal|
+      Goal.create(goal_name: goal, calendar_id: params[:calendar_id])
+    end
   end
 end
