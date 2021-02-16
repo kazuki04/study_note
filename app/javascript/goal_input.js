fbb269e1845e5df892d6e852c0233050b16eccb2
kazuki04 <kazukik0402@gmail.com>
2021-02-16 15:08:25 +0900

    add goal_inpu.js

--- app/javascript/goal_input.js
+++ app/javascript/goal_input.js
@@ -1,3 +1,31 @@
 $(function(){
+  build_input = function(i){
+    html = `
+      <div class="input_item">
+        <input name="goal[goal_names][]" id="goal_${i}" class="goal_input" type="text"> 
+        <a id="add_btn_${i}" class="goal_add_btn" data-index="${i}" href="#">追加</a>
+        <a id="delete_btn_${i}" class="goal_delete_btn" data-index="${i}" href="#">削除</a>
+      </div>`
+    
+    return html;
+  }
 
+
+  $(".input_field").on("click", ".goal_add_btn", function() {
+    if($(".input_field").children().length == 10){
+      return false;
+    } 
+
+    index = $(this).data("index")
+    if($(`#goal_${index}`).val() != ""){
+      $(".input_field").append(build_input(index + 1))
+    }
+  })
+
+  $(".input_field").on("click", ".goal_delete_btn", function(){
+    $(this).parent().remove();
+    if($(".input_field").children().length == 0){
+      $(".input_field").append(build_input(0)) 
+    }
+  })
 });
