e09cfc0dc9b0b5b38d5d1098d7b8621f9d8ce5c9
kazuki04 <kazukik0402@gmail.com>
2021-03-19 22:00:28 +0900

    add transaction_note_form

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,10 +3,4 @@ class Note < ApplicationRecord
   belongs_to :calendar
   has_many :note_tags
   has_many :tags, through: :note_tags
-
-  with_options presence: true do
-    validates :highlight
-    validates :excerpt
-    validates :body
-  end
 end
